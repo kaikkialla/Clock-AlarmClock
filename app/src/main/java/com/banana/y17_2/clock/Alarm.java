b5a1a6b031c7d0916576855cd5a15358c2cb8332
tiget <tigetxd@mail.com>
2018-10-15 21:02:28 +0300

    Добавил атрибуты, допилил часы(почти), почти дописал код для будильника

---
+++ app/src/main/java/com/banana/y17_2/clock/Alarm.java
@@ -0,0 +1,27 @@
+package com.banana.y17_2.clock;
+
+public class Alarm {
+    final String title;
+    final long timeStamp;
+    final long id;
+
+    public Alarm(String title, long timeStamp, long id) {
+        this.title = title;
+        this.timeStamp = timeStamp;
+        this.id = id;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+}
---
+++ app/src/main/java/com/banana/y17_2/clock/AlarmDataBase.java
@@ -0,0 +1,72 @@
+package com.banana.y17_2.clock;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+public class AlarmDataBase {
+    private List<Alarm> mAlarm;
+    private Context mContext;
+
+
+    public void initialize(Context context) {
+        this.mContext = context;
+    }
+
+
+    public void load() {
+        //TODO
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        final String s = preferences.getString("ALARMS", "");
+        final Gson gson = new Gson();
+        mAlarm = gson.fromJson(s, new TypeToken<List<Alarm>>(){}.getType());
+
+    }
+
+
+    public void save() {
+        //TODO
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        SharedPreferences.Editor editor = preferences.edit();
+        final Gson gson = new Gson();
+        final String s = gson.toJson(mAlarm, new TypeToken<List<Alarm>>(){}.getType());
+
+        editor.putString("ALARMS", s);
+        editor.apply();
+
+
+    }
+
+
+    public void addAlarm(Alarm alarm) {
+        //TODO
+
+        mAlarm.add(alarm);
+        save();
+    }
+
+
+    public void removeAlarm(long id) {
+        //TODO
+        boolean needsSave = false;
+        for(int i = 0; i < mAlarm.size(); i++) {
+            if(mAlarm.get(i).getId() == id) {
+                mAlarm.remove(i);
+                needsSave = true;
+                break;
+            }
+        }
+        if (needsSave) {
+            save();
+        }
+
+    }
+
+
+
+}
